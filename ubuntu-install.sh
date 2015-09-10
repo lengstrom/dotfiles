3095821a09e9fdbeba19c1b0dcb4e3e855a2e477
lengstrom <lengstrom@users.noreply.github.com>
2015-09-10 14:43:06 -0400

    added to install script

--- ubuntu-install.sh
+++ ubuntu-install.sh
@@ -5,4 +5,6 @@ apt-get install git-core
 
 wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh
 chsh -s `which zsh`
+cd ~
+wget https://raw.githubusercontent.com/lengstrom/dotfiles/master/.zshrc 
 
